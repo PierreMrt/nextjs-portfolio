783017bcacf6d92dcb7930281680b15b7a546a1b
PierreMrt <69766734+PierreMrt@users.noreply.github.com>
2025-11-29 18:24:43 +0100

    Update AnimatedBackground with floating squares moving upward

--- components/AnimatedBackground.tsx
+++ components/AnimatedBackground.tsx
@@ -1,15 +1,19 @@
 export default function AnimatedBackground() {
   return (
     <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
-      {/* Large floating blobs */}
-      <div className="absolute w-96 h-96 bg-white/10 rounded-full blur-3xl animate-blob top-0 -left-20"></div>
-      <div className="absolute w-96 h-96 bg-white/10 rounded-full blur-3xl animate-blob animation-delay-2000 top-0 -right-20"></div>
-      <div className="absolute w-96 h-96 bg-white/10 rounded-full blur-3xl animate-blob animation-delay-4000 bottom-0 left-20"></div>
-      <div className="absolute w-80 h-80 bg-white/5 rounded-full blur-2xl animate-blob animation-delay-3000 bottom-20 right-40"></div>
+      {/* Floating squares that move up and rotate */}
+      <div className="absolute w-32 h-32 bg-white/15 blur-sm animate-float-up-1 bottom-0 left-[10%] rotate-12"></div>
+      <div className="absolute w-24 h-24 bg-white/10 blur-sm animate-float-up-2 bottom-0 left-[25%] -rotate-6"></div>
+      <div className="absolute w-40 h-40 bg-white/12 blur-sm animate-float-up-3 bottom-0 left-[45%] rotate-45"></div>
+      <div className="absolute w-28 h-28 bg-white/15 blur-sm animate-float-up-4 bottom-0 right-[30%] -rotate-12"></div>
+      <div className="absolute w-36 h-36 bg-white/10 blur-sm animate-float-up-5 bottom-0 right-[15%] rotate-6"></div>
+      <div className="absolute w-20 h-20 bg-white/15 blur-sm animate-float-up-6 bottom-0 right-[5%] -rotate-45"></div>
       
-      {/* Additional smaller shapes for depth */}
-      <div className="absolute w-64 h-64 bg-white/5 rounded-full blur-2xl animate-blob animation-delay-1000 top-1/3 left-1/4"></div>
-      <div className="absolute w-72 h-72 bg-white/5 rounded-full blur-2xl animate-blob animation-delay-5000 top-1/2 right-1/4"></div>
+      {/* Additional squares for continuous effect */}
+      <div className="absolute w-32 h-32 bg-white/10 blur-sm animate-float-up-1 -bottom-32 left-[15%] rotate-12 animation-delay-8000"></div>
+      <div className="absolute w-28 h-28 bg-white/15 blur-sm animate-float-up-2 -bottom-32 left-[35%] -rotate-6 animation-delay-10000"></div>
+      <div className="absolute w-24 h-24 bg-white/12 blur-sm animate-float-up-3 -bottom-32 right-[40%] rotate-45 animation-delay-12000"></div>
+      <div className="absolute w-36 h-36 bg-white/10 blur-sm animate-float-up-4 -bottom-32 right-[20%] -rotate-12 animation-delay-14000"></div>
     </div>
   )
 }
