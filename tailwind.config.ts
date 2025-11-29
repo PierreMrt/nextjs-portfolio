c6ad5403e7be9b0742830cb140de779fe245f437
PierreMrt <69766734+PierreMrt@users.noreply.github.com>
2025-11-29 15:17:27 +0100

    Configure Tailwind Typography plugin for MDX content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,52 @@ const config: Config = {
         primary: '#0066CC',
         secondary: '#333333',
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: 'none',
+            color: '#333',
+            h2: {
+              fontWeight: '700',
+              fontSize: '1.5em',
+              marginTop: '2em',
+              marginBottom: '1em',
+            },
+            h3: {
+              fontWeight: '600',
+              fontSize: '1.25em',
+              marginTop: '1.6em',
+              marginBottom: '0.6em',
+            },
+            p: {
+              marginTop: '1.25em',
+              marginBottom: '1.25em',
+            },
+            strong: {
+              fontWeight: '600',
+              color: '#111',
+            },
+            ul: {
+              marginTop: '1.25em',
+              marginBottom: '1.25em',
+            },
+            li: {
+              marginTop: '0.5em',
+              marginBottom: '0.5em',
+            },
+            code: {
+              backgroundColor: '#f3f4f6',
+              padding: '0.2em 0.4em',
+              borderRadius: '0.25rem',
+              fontWeight: '600',
+            },
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
 export default config
