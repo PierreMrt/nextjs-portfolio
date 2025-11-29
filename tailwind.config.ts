99eda45b9a543102f47bcfb08361dccaa8cb81e1
PierreMrt <69766734+PierreMrt@users.noreply.github.com>
2025-11-29 18:12:05 +0100

    Add blob animation and animation delays to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,25 @@ const config: Config = {
           900: '#2f4f1a',
         },
       },
+      animation: {
+        blob: 'blob 7s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(30px, -50px) scale(1.1)',
+          },
+          '66%': {
+            transform: 'translate(-20px, 20px) scale(0.9)',
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+        },
+      },
       typography: {
         DEFAULT: {
           css: {
